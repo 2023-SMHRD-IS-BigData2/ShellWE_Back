fe6f1a6342561859303f3d7bc60db12ffde74275
smhrd <wkdwnsgb@gmail.com>
2024-01-20 09:42:25 +0900

    기본틀

---
+++ Roxi-1/src/main/java/com/smhrd/roxi/Roxi1Application.java
@@ -0,0 +1,13 @@
+package com.smhrd.roxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Roxi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Roxi1Application.class, args);
+	}
+
+}
---
+++ Roxi-1/src/test/java/com/smhrd/roxi/Roxi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smhrd.roxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Roxi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
