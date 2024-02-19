5ae4aa683499a4da2848fa61b47c9da62118dc3d
hyung <rudfufs11@gmail.com>
2024-02-19 10:39:29 +0900

    ssss

--- Roxi-1/src/main/java/com/smhrd/smart/flask/flask.java
+++ Roxi-1/src/main/java/com/smhrd/smart/flask/flask.java
@@ -23,29 +23,34 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.smhrd.smart.controller.CriteriaSepsissController;
 import com.smhrd.smart.controller.PatientController;
 import com.smhrd.smart.controller.VitalController;
+import com.smhrd.smart.entity.Smart_Patient;
+import com.smhrd.smart.repository.PatientRepository;
 
 @Controller
 public class flask {
-	final String URL = "http://localhost:7000/sepsis";
-	final String POST = "POST";
-	final String USER_AGENT = "Mozilla/5.0";
-	
-//	@Autowired
-//	private PatientController patientcontroller;
-	
-	@Autowired
-	private VitalController vitalcontroller;
-	
-	@Autowired
-	private CriteriaSepsissController criteriaseosiss;
-	
-	//환자 대표 sepsisscore 예측 함수
-	public String flask(List<HashMap<String, Object>> list, int patinum) throws IOException {
+   final String URL = "http://localhost:7000/sepsis";
+   final String POST = "POST";
+   final String USER_AGENT = "Mozilla/5.0";
+   
+//   @Autowired
+//   private PatientController patientcontroller;
+   
+   @Autowired
+   private VitalController vitalcontroller;
+   
+   @Autowired
+   private CriteriaSepsissController criteriaseosiss;
+   
+   @Autowired
+   private PatientRepository patientrepository;
+   
+   //환자 대표 sepsisscore 예측 함수
+   public String flask(List<HashMap<String, Object>> list, int patinum) throws IOException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-		
+      
         HttpEntity<List<HashMap<String, Object>>> requestEntity = new HttpEntity<>(list, headers);
-		
+      
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> responseEntity = restTemplate.exchange(URL, HttpMethod.POST, requestEntity, String.class);//flask 연동 메소드 호출
         
@@ -58,39 +63,42 @@ public class flask {
             System.err.println("Failed to send data. Status code: " + statusCode);
         }
         return responseEntity.getBody();//예측 값 리턴
-	}
-	
-	//세부 바이탈 예측 함수
-	public void flask_1(List<HashMap<String, Object>> list, int patinum, int vitalnum) throws IOException {
-		if (list!=null) {
-			for (int i = 0; i < list.size(); i++) {
-				System.out.println(list.get(i));
-			}
-		} else {
-			System.out.println("list가 비어있습니다.");
-		}
-		
+   }
+   
+   //세부 바이탈 예측 함수
+   public void flask_1(List<HashMap<String, Object>> list, int patinum, int vitalnum) throws IOException {
+      if (list!=null) {
+         for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+         }
+      } else {
+         System.out.println("list가 비어있습니다.");
+      }
+      
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-		
+      
         HttpEntity<List<HashMap<String, Object>>> requestEntity = new HttpEntity<>(list, headers);
-		
+      
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> responseEntity = restTemplate.exchange(URL, HttpMethod.POST, requestEntity, String.class);
         
         HttpStatus statusCode = responseEntity.getStatusCode();
+		Smart_Patient smartpatient = null;
         if (statusCode == HttpStatus.OK) {
             String responseBody = responseEntity.getBody();
-            
+           
             String result = vitalcontroller.setVitalDate(Integer.parseInt(responseBody),vitalnum); //환자 패혈증 수치 업데이트 메소드
-            criteriaseosiss.sepsissscoer(Integer.parseInt(responseBody));
+            smartpatient.setPatinum(patinum); // repository로 save할때 pk를 찾아가도록 환자번호 저장
+            smartpatient.setSepsisslevel(criteriaseosiss.sepsissscoer(Integer.parseInt(responseBody))); // db에 저장할 sepsisslevel확인후 저장
+            patientrepository.save(smartpatient); // jpa를 이용하여 db에 업데이트 하도록 수정
             System.out.println("Response from Flask server: " + responseBody);
             System.out.println("result : "+result);
         } else {
             System.err.println("Failed to send data. Status code: " + statusCode);
         }
         
-	}
-	
-	
+   }
+   
+   
 }
