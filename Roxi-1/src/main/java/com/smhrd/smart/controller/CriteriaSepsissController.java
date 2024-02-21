82efb5963a2f74ac582bde6f757db48cb23c2803
hyung <rudfufs11@gmail.com>
2024-02-21 10:00:16 +0900

    sepscoer ver3

--- Roxi-1/src/main/java/com/smhrd/smart/controller/CriteriaSepsissController.java
+++ Roxi-1/src/main/java/com/smhrd/smart/controller/CriteriaSepsissController.java
@@ -8,52 +8,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.smhrd.smart.entity.Smart_sepsiss;
 import com.smhrd.smart.repository.CriteriaSepsissRepository;
 
-@Controller
+@RestController // 리액트로 데이터만 전달하는 컨트롤러 어노테이션
 public class CriteriaSepsissController {
-	
-	@Autowired
-	private CriteriaSepsissRepository csrepo;
-	
-	@RequestMapping("/smartsepsiss") // sepsiss 정보를 전달하는 메소드
-	public ResponseEntity<JSONObject> sepsiss() {
-	    JSONObject responseJson = new JSONObject();
-	    try {
-	        Smart_sepsiss smartsepsiss = (Smart_sepsiss) csrepo.findAll();
-	        
-	        if (smartsepsiss != null) {
-	            responseJson.put("smartsepsiss", smartsepsiss);
-	            return new ResponseEntity<>(responseJson, HttpStatus.OK);
-	        } else {
-	            responseJson.put("error", "Smart sepsiss not found");
-	            return new ResponseEntity<>(responseJson, HttpStatus.NOT_FOUND);
-	        }
-	    } catch (Exception e) {
-	        responseJson.put("error", "Error while fetching Smart sepsiss");
-	        return new ResponseEntity<>(responseJson, HttpStatus.INTERNAL_SERVER_ERROR);
-	    }
-	}
-	
-	public String sepsissscoer(int sepsissscoer) {
-		String sepsissString ="";
-		List<Smart_sepsiss> sepsiss = csrepo.findAll();
-		if(sepsiss.get(0).getSepsiss() >= sepsissscoer) {
-			sepsissString="None";
-			System.out.println(sepsissString);
-			return sepsissString;
-		}
-		else {
-			sepsissString="Screening";
-			System.out.println(sepsissString);
-			return sepsissString;
-		}
-	}
-	
-	
+
+   @Autowired
+   private CriteriaSepsissRepository csrepo;
+
+   // front로 데이터 보내는 메소드
+   @RequestMapping("/getSep")
+   public JSONObject getSep() {
+      JSONObject json = new JSONObject();
+      List<Smart_sepsiss> list = csrepo.findAll();
+      json.put("sepscore", list.get(0).getSepsissnum());
+      return json;
+   }
+   
+   // DB로 정보를 업데이트
+   @RequestMapping("/sepsissscoer") // sepsiss 정보를 업데이트하는 메소드
+   public String CriteriaSepsiss(String sepsiss) {
+      List<Smart_sepsiss> list = csrepo.findAll();
+      System.out.println(list);
+      Smart_sepsiss sep = list.get(0); // 업데이트 전
+      sep.setSepsissnum(Integer.parseInt(sepsiss));
+      csrepo.save(sep);
+      return "";
+   }
+
+   //** 패혈증 기준에 의해 스크리닝으로 변환
+   public String sepsissscoer(int sepsissscoer) {
+      String sepsissString = "";
+      List<Smart_sepsiss> sepsiss = csrepo.findAll();
+      if (sepsiss.get(0).getSepsiss() >= sepsissscoer) {
+         sepsissString = "None";
+         System.out.println(sepsissString);
+         return sepsissString;
+      } else {
+         sepsissString = "Screening";
+         System.out.println(sepsissString);
+         return sepsissString;
+      }
+   }
+
 }
